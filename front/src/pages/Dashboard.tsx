f941dea1f175170de0d7732049c1ac3887d5e808
Will Dev <william.delepierre@gmail.com>
2024-08-23 10:07:42 +0200

    modifs dashboard

--- front/src/pages/Dashboard.tsx
+++ front/src/pages/Dashboard.tsx
@@ -4,23 +4,19 @@ import Products from "../components/Products";
 import Users from "../components/Users";
 import OrdersPage from "../pages/OrdersPage";
 import HomePage from "./HomePage";
-import PrivateRoute from "../components/PrivateRoute";
 
-export default function Dashboard({ isAuthenticated }: { isAuthenticated: boolean }) {
-
+export default function Dashboard() {
   return (
     <div className="flex grow">
       <div>
-      <Menu />
+        <Menu />
       </div>
       <div className="w-full">
         <Routes>
-          <Route element={<PrivateRoute isAuthenticated={isAuthenticated} />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/orders" element={<OrdersPage />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/users" element={<Users />} />
-          </Route>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/users" element={<Users />} />
         </Routes>
       </div>
     </div>
