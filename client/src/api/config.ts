45a06372613b246d2136f911f6d99366fb0f37e3
Aurelien_Dornier <164883073+Aurelien-Dornier@users.noreply.github.com>
2024-11-12 17:45:55 +0100

    Update config.ts

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,13 +1,1 @@
-// config.ts
-interface Environment {
-  PROD: boolean;
-  VITE_API_URL?: string;
-}
-
-declare global {
-  interface ImportMetaEnv extends Environment {}
-}
-
-export const apiBaseUrl = import.meta.env.PROD 
-  ? (import.meta.env.VITE_API_URL || "https://kiboude-pepin-api.onrender.com")
-  : "http://localhost:3000";
+export const apiBaseUrl = "https://kiboude-pepin-api.onrender.com";
